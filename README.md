8efb545ce6f4004ff8f3df48e2082ceaad1ac300
zhangzhenyang <zhangzhenyang@baidu.com>
2017-05-02 19:21:00 +0800

    update readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Odux
+An attempt to observable redux.
+
+# Example
+[https://github.com/zhang740/odux-example](https://github.com/zhang740/odux-example)
--- dist/SpyEvent.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const event_1 = require("./event");
-class SpyEvent extends event_1.BaseEvent {
-}
-exports.SpyEvent = SpyEvent;
-//# sourceMappingURL=SpyEvent.js.map
