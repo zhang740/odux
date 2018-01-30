c617b0e5038276c9baa9212f49ecf5798624d1d3
zhang740 <zhang740@vmzclub.com>
2018-01-30 17:31:53 +0800

    update: change code folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
-
 module.exports = {
-  entry: {
-    odux: [
-      path.resolve(__dirname, 'lib/index.js'),
+    entry: {
+        odux: [
+            path.resolve(__dirname, 'lib/index.js'),
+        ],
+        'power-di': ['power-di']
+    },
+    output: {
+        filename: '[name].min.js',
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: 'umd',
+    },
+    module: {},
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
     ],
-    "power-di": ['power-di']
-  },
-
-  output: {
-    filename: '[name].min.js',
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'umd',
-  },
-
-  module: {
-  },
-
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    }),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin()
-  ],
-}
+};
+//# sourceMappingURL=webpack.config.js.map
---
+++ webpack.config.ts
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    odux: [
+      path.resolve(__dirname, 'lib/index.js'),
+    ],
+    'power-di': ['power-di']
+  },
+
+  output: {
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+  },
+
+  module: {
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+};
